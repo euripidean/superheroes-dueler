a1614d33cb4b51d2992749a9a6455a325bedd6f6
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2022-11-09 17:20:18 -0800

    added abilities and armor

---
+++ armor.py
@@ -0,0 +1,24 @@
+"""
+Armor Class
+"""
+import random
+
+class Armor:
+    def __init__(self, name, max_block):
+        '''
+        Initialize the values passed into this
+        method as instance variables.
+        '''
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        random_value = random.randint(0,self.max_block)
+        return random_value
+
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+    armor = Armor("Debugging Shield", 10)
+    print(armor.name)
+    print(armor.block())
